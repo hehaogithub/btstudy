90bc7801052aeb0d6bfedc5c53292595607adbd0
hehaogithub <1757300860@qq.com>
2019-12-26 16:18:57 +0800

    更新

--- src/main/java/com/hh/boot/controller/HelloController.java
+++ src/main/java/com/hh/boot/controller/HelloController.java
@@ -5,6 +5,10 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @Api(value="Hello控制器配置",tags="xxx")
 public class HelloController {
@@ -14,5 +18,13 @@ public class HelloController {
 	@ApiOperation(value ="测试json",notes ="无输入参数" )
 	public String sayHello() {
 		return "login";
+	}
+	@RequestMapping("/json")
+	@ResponseBody
+	public Map<String,String> getJson(){
+		Map<String,String> map=new HashMap<>();
+		map.put("name","小明");
+		map.put("age","23");
+		return map;
 	}
 }
